888d9d4bf1d7773495dc822ceff97961f496e299
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2021-12-13 19:16:10 -0800

    Updated constructor/ Added destructor

--- bank.cpp
+++ bank.cpp
@@ -8,12 +8,17 @@ class Account{
         std::string bankName;
         bool checking;
 
-    Account(float x, string y, bool z){
+    Account(float x, std::string y, bool z){
+        if (x < 0){
+            std::cout << "Balance must be > 0" << std::endl;
+        } else{
         balance= x;
         bankName= y;
-        checking= z;
+        checking= z;};
 
     }
+
+    ~Account();
 
     void Deposit(float amount){
         balance += amount;
@@ -22,7 +27,7 @@ class Account{
     float Withdrawal(float amount){
         if (amount < balance){
             balance -= amount;
-            return amount
+            return amount;
         } else {
             std::cout << "Not enough funds in account"<< std::endl;
         };
