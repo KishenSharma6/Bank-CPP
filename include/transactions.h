df87fea886df25f495ca89f2abd4f39e1fd10c59
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2022-01-18 15:13:32 -0800

    Removed constructor

--- include/transactions.h
+++ include/transactions.h
@@ -2,18 +2,10 @@
 #define transactions_h
 
 #include <iostream>
-//contain function/class declaration
-class Account{
-public:
-    float balance;
-    std::string bankName;
-    
-    // Account(float x, std::string y);
-    int deposit(float amount);
 
-    int withdraw(float amount);
+// Account(float x, std::string y);
+int deposit(float amount);
+int withdraw(float amount);
+void print_receipt(float balance);
 
-    void print_receipt();
-
-};
 #endif
