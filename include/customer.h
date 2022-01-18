fe3da0c0d01178847b89f0e93c7640b3f19804fa
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2022-01-18 06:57:47 -0800

    Added visit_bank()

--- include/customer.h
+++ include/customer.h
@@ -8,9 +8,11 @@ class Customer{
         int atm_pin= 1234;
     public:
         std::string name;
+        float wallet = 0.00;
         
 
-    Customer(int a, std::string b);
+    Customer(std::string a, float b);
+    void visit_bank();
 
 };
     
