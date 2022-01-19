b35d50975ab991a24d2cfe0c0efd77997e5ebf8c
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2022-01-19 09:43:34 -0800

    Removed Customer constructor \n Pivoting project. Will create bank app that interacts directly with user \n that will store data created by customer

--- include/customer.h
+++ include/customer.h
@@ -3,18 +3,9 @@
 
 #include <iostream>
 
-class Customer{
-    private:
-        int atm_pin= 1234;
-    public:
-        std::string name;
-        float wallet = 0.00;
-        
-
-    Customer(std::string a, float b);
-    void visit_bank();
+void visit_bank();
 
 
-};
+
     
 #endif
--- src/customer.cpp
+++ src/customer.cpp
@@ -2,19 +2,10 @@
 #include "../include/customer.h"
 #include "../include/machine.h"
 
- Customer::Customer(std::string a, float b){
-     
-    name= a;
-    wallet = b;
-    int verified_pin= create_pin();
-    atm_pin= verified_pin;
 
-    
 
- }
-
- void Customer::visit_bank(){
-    std::cout << "Would  " << name << " like to visit the ATM (Y/N)?" << std::endl;
+ void visit_bank(){
+    std::cout << "Would you like to visit the ATM (Y/N)?" << std::endl;
     char response;
     std::cin >> response;
 
@@ -23,7 +14,7 @@
         std::cin >> response;
         
         if (toupper(response) == 'N'){
-            std::cout << "Have a nice day " << name << " !" << std::endl;
+            std::cout << "Have a nice day!" << std::endl;
         } else if (toupper(response) == 'Y'){
             //initiate welcome sequence from machine.cpp
             //welcome();
