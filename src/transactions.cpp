a6eb2b7a79c64627b61362568ad5202ce3b074b7
KishenSharma6 <57292990+KishenSharma6@users.noreply.github.com>
2022-02-06 09:00:45 -0800

    Updated deposit() to verify user pin and account

--- src/transactions.cpp
+++ src/transactions.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include "../include/transactions.h"
+#include "../include/machine.h"
 
-// Account::Account(float x, std::string y){
-//     balance= x;
-//     bankName= y;
-// }
 int deposit(){
+    /*Deposit funds into user's account*/
+    int user_acct = 0;
+    std::cout << "Please enter your 5-digit account number: " << std::endl;
+    std::cin >> user_acct;
+
+    /*Verify this account exists*/
+    if (verify_account(user_acct) == 0)
+    {
+        std::cout<< "Incorrect account number" << std::endl;
+        deposit();
+    }
+    /*Verify pin*/
+    int user_pin= 0;
+    std::cout << "Please enter your 4-digit pin:" << std::endl;
+    std::cin >> user_pin;
+
+    if (pin_verification(std::to_string(user_acct), std::to_string(user_pin)) == 0)
+    {
+        std::cout << "Pin is incorrect. Make sure you have the correct account number and try again" << std::endl;
+        deposit();
+    }
+
+    /* Get deposit amount from user*/
+    float deposit_amount= 0.00;
+
     return 0;
 }
 
